e8479e0dc1eda2194c2ec05a07278b8858a494c4
TaeUkChu <62568938+TaeUkChu@users.noreply.github.com>
2024-03-29 15:44:35 +0900

    0329

---
+++ code_ctu/BaekJoon/Sort/11651_좌표 정렬하기2.py
@@ -0,0 +1,51 @@
+'''
+문제
+2차원 평면 위의 점 N개가 주어진다.
+좌표를 y좌표가 증가하는 순으로,
+y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음
+출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다.
+둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다.
+(-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고,
+위치가 같은 두 점은 없다.
+
+출력
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+예제 입력 1
+5
+0 4
+1 2
+1 -1
+2 2
+3 3
+예제 출력 1
+1 -1
+1 2
+2 2
+3 3
+0 4
+
+Sol) 접근 방식
+x좌표 정렬 후 y좌표 정렬하면 되지 않나?
+'''
+N = int(input())
+cord = []
+def sort_x(value):
+    return value[0]
+def sort_y(value):
+    return value[1]
+def print_answer(cord):
+    for i in range(len(cord)) :
+        print(cord[i][0],cord[i][1])
+
+for _ in range(N):
+    x,y = map(int,input().split())
+    cord.append([x,y])
+
+cord.sort(key=sort_x)
+cord.sort(key=sort_y)
+
+print_answer(cord)
