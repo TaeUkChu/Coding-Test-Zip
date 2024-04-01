42d8dbc37bb7e4cbbe8dc1d2bb5861a9fb0b8bb5
TaeUkChu <62568938+TaeUkChu@users.noreply.github.com>
2024-04-01 22:28:29 +0900

    0401 Baekjoon

--- code_java/BaekJoon/Basic/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-// 2557번 문제
-class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("hello World");
-    }
-}
---
+++ code_java/BaekJoon/Basic/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package HelloWorld;
+
+// 2557번 문제
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
