11ec1f0b028caa143208fa79b05026aebcd951d3
TaeUkChu <62568938+TaeUkChu@users.noreply.github.com>
2024-04-17 21:19:34 +0900

    0417

---
+++ code_java/BaekJoon/Basic/반복문/구구단/Main.java
@@ -0,0 +1,14 @@
+package 반복문.구구단;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+
+        for (int i=1; i<10; i++){
+            System.out.println( N + " * " + i + " = " + N*i);
+        }
+    }
+}
