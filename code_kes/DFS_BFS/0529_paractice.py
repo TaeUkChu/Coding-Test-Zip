c1d1efb635c0a6b9321d45b7dc2f493a5750105f
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-05-29 10:44:32 +0900

    프로그래머스 깊이/너비 우선 탐색(DFS/BFS) 풀이 정리 미완료

---
+++ code_kes/DFS_BFS/0529_paractice.py
@@ -0,0 +1,18 @@
+# from queue import Queue
+
+# q = Queue([1,1,1,1,1])
+# a = q.get()
+# print(a)
+
+from collections import deque
+number = [1,1,1,1,1]
+target = 3
+answer = 0
+q =deque()
+n = len(number)
+
+q.append([number[0],0])
+
+
+
+
