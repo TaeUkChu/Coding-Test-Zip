adc77333c9849cdc2ad11e465ea24b524879af96
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-06-24 13:30:04 +0900

    요세푸스문제0

---
+++ code_kes/solved/class2/요세푸스문제0.py
@@ -0,0 +1,28 @@
+# 문제 : https://www.acmicpc.net/problem/11866
+
+n, k = map(int, input().split(" "))
+from collections import deque
+         
+q = deque(range(1, n+1))
+
+ans = []
+while q:
+    for i in range(k-1):
+        q.append(q.popleft())        
+    ans.append(q.popleft())
+        
+ans = "<" + ", ".join(map(str, ans)) + ">"
+print(ans)
+
+
+ans = []
+while q:
+    for i in range(k):
+        if i == 2: 
+            temp = q.popleft()
+            ans.append(temp)
+        else:
+            temp = q.popleft()
+            q.append(temp)
+ans = "<" + ", ".join(map(str, ans)) + ">"
+print(ans)
