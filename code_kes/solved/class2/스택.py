c368dc0a369725a6161a27c28b0519fbd16ebc4e
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-07-04 10:39:46 +0900

    스택 풀이 완료 정리 미완료

---
+++ code_kes/solved/class2/스택.py
@@ -0,0 +1,48 @@
+# 문제 : https://www.acmicpc.net/problem/10828
+import sys 
+N = int(sys.stdin.readline())
+
+class Stack:
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity
+        self.array = [None]*capacity
+        self.idx = -1
+        
+    def push(self, item): # 삽입
+        if not self.idx == self.capacity-1: # stack이 꽉차 있지 않으면
+            self.idx += 1
+            self.array[self.idx] = item
+        else: pass
+    
+    def pop(self):
+        if not self.empty():
+            item = self.array[self.idx]
+            self.idx -= 1
+            return item
+        else: 
+            return -1
+        
+    def size(self): return self.idx+1
+    
+    def empty(self):
+        if self.idx == -1:
+            return 1
+        else:
+            return 0
+    
+    def top(self):
+        if not self.empty():
+            return self.array[self.idx]
+        else:
+            return -1
+    
+
+s = Stack(N)
+for i in range(N):
+    ord_li = sys.stdin.readline().split()
+    
+    if len(ord_li) == 2:
+        getattr(s,ord_li[0])(int(ord_li[1]))
+    else:
+        print(getattr(s,ord_li[0])())
+
