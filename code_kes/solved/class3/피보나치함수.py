abf9c89c19b3594e161c828677c37a8f28744c19
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-07-14 17:14:57 +0900

    백준 피보나치 함수 실버3 풀이

---
+++ code_kes/solved/class3/피보나치함수.py
@@ -0,0 +1,53 @@
+# https://www.acmicpc.net/problem/1003
+
+import sys
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+memo = {
+    0 : 0,
+    1 : 1,
+    2 : 1,
+}
+
+def fb(temp):
+    if not temp in memo:
+        memo[temp] = fb(temp-1) + fb(temp-2)
+    return memo[temp]
+
+n = int(input())
+for i in range(n):
+    temp = int(input())
+    if not temp:
+        print('1 0')
+    else:
+        fb(temp)
+        print(memo[temp-1], memo[temp])
+
+
+
+
+
+# if n == 0:
+        
+#         zero += 1
+#         return 0
+    
+#     elif n == 1:
+#         one += 1
+#         return 1
+    
+#     else:
+#         return fb(n-1) + fb(n-2)
+
+# n = int(input())
+
+
+
+# for i in range(n):
+#     one = 0
+#     zero = 0
+#     a = int(input())
+#     fb(a)
+#     print(zero, one)
+    
